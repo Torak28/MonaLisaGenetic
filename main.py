36776dae41436ee2ee2085b1d66b5c2e6e5ac4b9
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-09-23 16:02:20 +0200

    Twi transpawncy polygon

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
 # Open Lisa and create other image
 og = Image.open('MonaLisa.png')
 widthOG, heightOG = og.size
-black = Image.new('RGB', (widthOG, heightOG), 255)
+black = Image.new('RGB', og.size)
+
+draw = ImageDraw.Draw(black, 'RGBA')
+draw.polygon([(50, 0), (100, 100), (0, 100)], (255, 0, 0, 125))
+draw.polygon([(50,100), (100, 0), (0, 0)], (0, 255, 0, 125))
+del draw
+
 black.show()
-black.save('out.png')
+# black.save('out.png')
