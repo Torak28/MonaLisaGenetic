4b8aac0a8a56c2e0d5bd87c9eb3ba8ec3e0ab9dc
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-10-03 14:26:28 +0200

    Testing writing bytes

--- testArray.py
+++ testArray.py
@@ -7,7 +7,9 @@ im = Image.open("MonaLisa.png").convert("RGBA")
 # convert to numpy (for convenience)
 tab = numpy.asarray(im)
 
-f = open('test.txt', 'w')
-for item in tab:
-    f.write("%s\n" %item)
-f.close()
+#f = open('test.txt', 'w')
+#for item in tab:
+#    f.write("%s\n" %item)
+#f.close()
+
+print(tab[0][0][0])
