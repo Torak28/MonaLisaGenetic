6f16688b15301a03fe9ab69877d0a082faba3869
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-10-03 13:26:47 +0200

    Testing writing bytes

---
+++ testArray.py
@@ -0,0 +1,13 @@
+import numpy
+from PIL import Image
+
+# read image as RGB and add alpha (transparency)
+im = Image.open("MonaLisa.png").convert("RGBA")
+
+# convert to numpy (for convenience)
+tab = numpy.asarray(im)
+
+f = open('test.txt', 'w')
+for item in tab:
+    f.write("%s\n" %item)
+f.close()
