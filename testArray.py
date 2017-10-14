807e25b64efb327f6f4d4878239845d65fc7fa8a
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-10-14 18:31:52 +0200

    Test distance

--- testArray.py
+++ testArray.py
@@ -1,4 +1,5 @@
 import numpy
+import math
 from PIL import Image
 
 # read image as RGB and add alpha (transparency)
@@ -24,6 +25,18 @@ def darkPicture(N):
     ret = numpy.zeros_like(N)
     return ret
 
+# distance between 2 oints in 3D space
+# |P_1\, P_2| = \sqrt{(x_2- x_1)^2 + (y_2 -y_1)^2 + (z_2- z_1)^2}
+def distance(org, N):
+    ret = 0
+    for i in range(org.shape[0]):
+        for j in range(org.shape[1]):
+            ret += math.sqrt(pow(N[i][j][0] - org[i][j][0],2) + pow(N[i][j][1] - org[i][j][1],2) + pow(N[i][j][2] - org[i][j][2],2))
+    return ret
+
+
+
 newIm = Image.fromarray(darkPicture(tab), "RGBA")
+print(tab[0][0])
 newIm.show()
 newIm.save("out3.png")
