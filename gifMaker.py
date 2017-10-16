94936233689a5d7bbe2fbe3e5aa3e02e01ee04ad
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-10-16 17:03:52 +0200

    Add gif maker from run

---
+++ gifMaker.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import os
+
+path = 'E:/INZ/'
+len = len(os.listdir(path))
+
+im = []
+Im = Image.open("Gif.gif")
+
+for i in range(len):
+    im.append(Image.open(path + str(i) + ".png"))
+
+
+Im.save("Gif.gif", save_all=True, append_images=[img for img in im], loop=1, duration=500)
