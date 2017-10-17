b6fe8374822c1d50a27e699b2f4ce24dcfecb36e
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-10-17 02:32:04 +0200

    3rd done, setup for 4th

--- testArray.py
+++ testArray.py
@@ -48,7 +48,8 @@ def square(org, N):
     R = org[x][y][0]
     G = org[x][y][1]
     B = org[x][y][2]
-    A = numpy.random.randint(256)
+    # eksperymentalne wydanie
+    A = numpy.random.randint(126)
     for i in range(N.shape[0]):
         if i >= x and i <= w:
             for j in range(N.shape[1]):
@@ -69,10 +70,13 @@ def mapFromTo(x,a,b,c,d):
    ret = (x - a) /(b - a) * (d - c) + c
    return ret
 
-def matingpool(pop, pop_size):
+def matingpool(pop, pop_size, maxFit):
+    pop = sorted(pop, key=lambda x:(x['fit']))
     ret = []
     for k in range(pop_size):
-        kon = int(pop[k]['fit'] * 100)
+        # Rzutowanie ilosci elementow z aktalnego fita na zakres 100 do 0
+        # Dzięki temu mainting pool zawiera więcej dobrych osobników
+        kon = int(mapFromTo(pop[k]['fit'], 0, maxFit, 100, 0))
         for l in range(kon):
             ret.append(populacja[k])
     return ret
@@ -83,25 +87,25 @@ def mutate(pop, pop_size, pop_it, org):
             pop[i]['tab'] = square(org, pop[i]['tab'])
     return pop
 
-def score(pop, pop_size, maxFit):
+def score(pop, pop_size):
     for i in range(pop_size):
-        pop[i]['fit'] = mapFromTo(distance2(tab, pop[i]['tab']), 0, maxFit, 0, 1)
+        pop[i]['fit'] = distance2(tab, pop[i]['tab'])
     return pop
 
-def crossover(pop, pool):
+def crossover(pop, pool, maxFit):
     ret = []
     child = {}
     for i in range(len(pop)):
         parentA = numpy.random.choice(pool)
         parentB = numpy.random.choice(pool)
         child['tab'] = Add(parentA['tab'], parentB['tab'])
-        child['fit'] = 100
+        child['fit'] = maxFit
         ret.append(child)
     return ret
 
-def dump_best(pop, pop_size,  it):
+def dump_best(pop, it):
     best = pop[0]
-    for i in range(pop_size):
+    for i in range(len(pop)):
         if pop[i]['fit'] < best['fit']:
             best = pop[i]
     newIm = Image.fromarray(best['tab'], "RGBA")
@@ -118,13 +122,14 @@ tab = numpy.asarray(im, dtype='uint8')
 
 # Sterowanie
 ilosc_w_populacji = 50
-ilosc_petli = 1000
-wspolczynnik_mutacji = 0.01
+ilosc_petli = 10000
+wspolczynnik_mutacji = 0.1
 
 populacja = []
 
 dark = darkPicture(tab)
 fit = distance2(tab, dark)
+maxFit2 = pow(765,2) * dark.shape[0] * dark.shape[1]
 
 
 # Tworzenie N osobnikow losowych
@@ -136,10 +141,14 @@ for p in range(ilosc_petli):
     # Mutacja
     populacja = mutate(populacja, ilosc_w_populacji, wspolczynnik_mutacji, tab)
     # Ocena( 0 - 100 )
-    populacja = score(populacja, ilosc_w_populacji, fit)
-    # Zrzucanie najlepszego w populacji
-    dump_best(populacja, ilosc_w_populacji, p)
+    populacja = score(populacja, ilosc_w_populacji)
     # Tworzenie poli rozrodczej do krzyżowania
-    pola_rozrodcza = matingpool(populacja, ilosc_w_populacji)
+    pola_rozrodcza = matingpool(populacja, ilosc_w_populacji, maxFit2)
     # Krzyzowanie i nowa populacja
-    populacja = crossover(populacja, pola_rozrodcza)
+    populacja = crossover(populacja, pola_rozrodcza, maxFit2)
+    # Zrzucanie najlepszego w populacji
+    dump_best(pola_rozrodcza, p)
+
+
+    # Zmieniłem maiting poola z losowanie Alphy na 125.
+    # Można pokminić z lepszym rzutowaniem i samym krzyżowaniem jako takim
