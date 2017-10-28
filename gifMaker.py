147d78b3139b94c0be57c4d3d0dd26ac0e01625f
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-10-28 20:14:05 +0200

    9th run

--- gifMaker.py
+++ gifMaker.py
@@ -1,16 +1,16 @@
 from PIL import Image
 import os
 
-path = 'E:/INZ/'
+path = 'E:/INZ4/'
 len = len(os.listdir(path))
 
 im = []
-Im = Image.open(path + "5001.png")
+Im = Image.open(path + "0.png")
 
-for i in range(5001,len):
+for i in range(len):
     im.append(Image.open(path + str(i) + ".png"))
 
 
 imgList = [img for img in im]
 
-Im.save("8bth_run.gif", save_all=True, append_images=imgList, loop=1, duration=10)
+Im.save("9th_run.gif", save_all=True, append_images=imgList, loop=1, duration=10)
