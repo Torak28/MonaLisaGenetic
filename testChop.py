21d6796b7028a0a426a67e90015eeb612a9d6c37
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-11-02 15:46:27 +0100

    add pre v4

--- testChop.py
+++ testChop.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageChops, ImageStat, ImageDraw
 
 img1 = Image.open("MonaLisa.png").convert("RGBA")
-img2 = Image.open("E:\INZ5v3\\29416.png").convert("RGBA")
+img2 = Image.open("E:\INZ5v3\\65531.png").convert("RGBA")
 
 h = ImageChops.subtract(img1, img2).show()
