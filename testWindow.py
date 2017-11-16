a1956e55c8508f68f9dc479e74dde355e44d7e2f
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-11-16 17:50:13 +0100

    polygon done

--- testWindow.py
+++ testWindow.py
@@ -14,7 +14,7 @@ def ellipse(org, N):
         h = random.randint(y + 1, org.size[1] + 20)
     else:
         h = random.randint(y + 1, org.size[1] + 20)
-    col = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+    col = find_median_colorE(org, x, y, w, h)
     R = col[0]
     G = col[1]
     B = col[2]
@@ -37,7 +37,7 @@ def square(org, N):
         h = random.randint(y + 1, org.size[1] + 20)
     else:
         h = random.randint(y + 1, org.size[1] + 20)
-    col = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+    col = find_median_colorS(org, x, y, w, h)
     R = col[0]
     G = col[1]
     B = col[2]
@@ -47,12 +47,45 @@ def square(org, N):
     N.paste(tmp,mask=tmp)
     return N
 
+def polygon(org, N):
+    tmp = Image.new('RGBA', N.size)
+    ret = ImageDraw.Draw(tmp)
+    tup = ()
+    for i in range(0, 3):
+        x = random.randint(-20, org.size[0] + 20)
+        y = random.randint(-20, org.size[1] + 20)
+        tup += ((x,y),)
+    col = find_median_colorP(org, tup)
+    R = col[0]
+    G = col[1]
+    B = col[2]
+    # eksperymentalne wydanie
+    A = random.randint(0, wartosc_alphy)
+    ret.polygon(tup, fill=(R, G, B, A))
+    N.paste(tmp, mask=tmp)
+    return N
 
 def find_median_colorS(N, x, y, w, h):
     tmp = Image.new('L', N.size)
     ret = ImageDraw.Draw(tmp)
 
     ret.rectangle(((x, y), (w, h)), outline= 1, fill=1)
+    median = ImageStat.Stat(N, mask=tmp).median
+    return median
+
+def find_median_colorP(N, tup):
+    tmp = Image.new('L', N.size)
+    ret = ImageDraw.Draw(tmp)
+
+    ret.polygon(tup, outline= 1, fill=1)
+    median = ImageStat.Stat(N, mask=tmp).median
+    return median
+
+def find_median_colorE(N, x, y, w, h):
+    tmp = Image.new('L', N.size)
+    ret = ImageDraw.Draw(tmp)
+
+    ret.ellipse(((x, y), (w, h)), outline= 1, fill=1)
     median = ImageStat.Stat(N, mask=tmp).median
     return median
 
@@ -63,9 +96,15 @@ def darkPicture(N):
 
 wartosc_alphy = 126
 ideal = Image.open("MonaLisa.png").convert("RGBA")
-dark = darkPicture(ideal)
+dark1 = darkPicture(ideal)
+dark2 = darkPicture(ideal)
+dark3 = darkPicture(ideal)
 
-for i in range(40):
-    dark = square(ideal, dark)
+for i in range(2000):
+    dark1 = polygon(ideal, dark1)
+    dark2 = ellipse(ideal, dark2)
+    dark3 = square(ideal, dark3)
 
-dark.show()
+dark1.show()
+dark2.show()
+dark3.show()
