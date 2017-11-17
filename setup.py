cec8e899bd46f589d26b51542a133b2816ab4571
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-11-17 14:50:07 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='MonaLisaGenetic',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Torak28',
+    author_email='',
+    description=''
+)
