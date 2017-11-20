ecc872106712a92399663dd50e68fbd937246df7
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-11-20 14:15:19 +0100

    Some improvements 2

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='MonaLisaGenetic',
-    version='',
+    version='0.1',
     packages=[''],
-    url='',
-    license='',
+    url='https://github.com/Torak28/MonaLisaGenetic',
+    license='MIT',
     author='Torak28',
-    author_email='',
+    author_email='218386@student.pwr.edu.pl',
     description=''
 )
